10985e6d72d2bb1f0943b33f8c39864c256d1703
dc-choi <cheek0805@naver.com>
2021-09-18 19:51:39 +0900

    sample 쉘 스크립트 작성

---
+++ sample.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#: Title	:	Sample bash script
+#: Date		:	2021-09-18
+#: Author	:	Choi Dong Chul <cheek0805@naver.com>
+#: Version	:	1.0
+#: Description	:	
+
+echo "=============================="
+echo "Today : $(date +%Y-%m-%d)"
+echo "=============================="
+echo "Hello, Linux Shell world!"
+echo "=============================="
+df -h
+echo "=============================="
