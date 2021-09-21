328d236c784e2ae7da03d3495faac39fda5ea91e
dc-choi <cheek0805@naver.com>
2021-09-21 07:48:33 +0000

    Positional Parameters 예시

---
+++ pp.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#: Title	:	example Positional Parameters
+#: Date		:	2021-09-21
+#: Author	:	Choi Dong Chul <cheek0805@naver.com>
+#: Version	:	1.0
+#: Description	:	
+
+echo "The script name : $0"
+echo "The First argument : $1"
+echo "The second argument : $2"
+echo "The number of argument : $#"
+echo "The list of argument : $@"
+echo "The list of argument : $*"
