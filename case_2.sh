3e0aa9003fd00d43d93eabb7ee92733b62933628
dc-choi <cheek0805@naver.com>
2021-09-22 11:43:18 +0000

    case_2 실습 예제 쉘 스크립트 작성

--- case_2.sh
+++ case_2.sh
@@ -7,3 +7,19 @@
 #: Description	:	
 
 cat << END
+====================
+Please select a number
+--------------------
+1 : Check disk usase
+2 : Check the login user list
+--------------------
+END
+echo -n "number : "
+read number
+case $number in
+    1) df -h;;
+    2) who;;
+    *) echo "Bad Choice!"
+    exit 1;;
+esac
+exit 0
